88ca672e45f6c9c341d93cd4eadcc517fd65507f
anzo-p <anssi.pitkan@gmail.com>
2024-01-20 00:38:45 +0200

    persistence - ws handler dynamodb table

--- persistence/dynamodb/lib/dynamodb-stack.ts
+++ persistence/dynamodb/lib/dynamodb-stack.ts
@@ -6,14 +6,27 @@ export class DynamodbStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new dynamodb.Table(this, "ControlTowerWebsocketConnectionsTable", {
-      tableName: "ControlTowerWebsocketConnections",
+    const table = new dynamodb.Table(
+      this,
+      "ControlTowerWebsocketConnectionTable",
+      {
+        tableName: "ControlTowerWebsocketConnectionTable",
+        partitionKey: {
+          name: "connectionId",
+          type: dynamodb.AttributeType.STRING,
+        },
+        billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+        removalPolicy: cdk.RemovalPolicy.RETAIN,
+      }
+    );
+
+    table.addGlobalSecondaryIndex({
+      indexName: "ControlTowerWebsocketGetConnectionsBySymbolIndex",
       partitionKey: {
-        name: "connectionId",
+        name: "symbol",
         type: dynamodb.AttributeType.STRING,
       },
-      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
-      removalPolicy: cdk.RemovalPolicy.RETAIN,
+      projectionType: dynamodb.ProjectionType.KEYS_ONLY,
     });
   }
 }
