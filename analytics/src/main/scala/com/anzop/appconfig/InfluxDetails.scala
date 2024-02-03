2d7b951ce6828aabd311a9032e848eb85ab5275f
anzo-p <anssi.pitkan@gmail.com>
2024-02-03 10:08:24 +0200

    analytics - ren to INFLUXDB_READ_TOKEN

--- analytics/src/main/scala/com/anzop/appconfig/InfluxDetails.scala
+++ analytics/src/main/scala/com/anzop/appconfig/InfluxDetails.scala
@@ -12,7 +12,7 @@ object InfluxDetails {
     val baseUrl   = sys.env.getOrThrow("INFLUXDB_URL", "INFLUXDB_URL is not set")
     val org       = sys.env.getOrThrow("INFLUXDB_ORG", "INFLUXDB_ORG is not set")
     val bucket    = sys.env.getOrThrow("INFLUXDB_BUCKET", "INFLUXDB_BUCKET is not set")
-    val token     = sys.env.getOrThrow("INFLUXDB_TOKEN", "INFLUXDB_TOKEN is not set").replace("\"", "")
+    val token     = sys.env.getOrThrow("INFLUXDB_WRITE_TOKEN", "INFLUXDB_TOKEN is not set").replace("\"", "")
     val uriPrefix = s"$baseUrl/api/v2/write?org=$org"
 
     InfluxDetails(new URL(uriPrefix), bucket, token)
