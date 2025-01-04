1bb2a8619bae48fb4536092f99ab024af034885f
anzo-p <anssi.pitkan@gmail.com>
2025-01-04 15:35:45 +0200

    currents - Drawdown handling of last day

--- currents/src/main/scala/net/anzop/processors/Drawdown/DrawdownProcessor.scala
+++ currents/src/main/scala/net/anzop/processors/Drawdown/DrawdownProcessor.scala
@@ -6,28 +6,49 @@ import org.apache.flink.configuration.Configuration
 import org.apache.flink.streaming.api.functions.KeyedProcessFunction
 import org.apache.flink.util.Collector
 
-class DrawdownProcessor extends KeyedProcessFunction[String, MarketData, Drawdown] {
-  private val startingValue: Double = 0.0
+import java.time.Instant
+import java.time.temporal.ChronoUnit
 
-  private var maxValueState: ValueState[Double] = _
+class DrawdownProcessor extends KeyedProcessFunction[String, MarketData, Drawdown] {
+  private val logger = org.slf4j.LoggerFactory.getLogger(getClass)
+
+  private val startingState: (Long, Double)             = (0L, 0.0)
+  private var maxValueState: ValueState[(Long, Double)] = _
 
   override def open(parameters: Configuration): Unit = {
-    val descriptor = new ValueStateDescriptor[Double]("maxValue", classOf[Double])
+    val descriptor = new ValueStateDescriptor[(Long, Double)]("maxValueState", classOf[(Long, Double)])
     maxValueState = getRuntimeContext.getState(descriptor)
   }
 
   override def processElement(
-      value: MarketData,
+      elem: MarketData,
       ctx: KeyedProcessFunction[String, MarketData, Drawdown]#Context,
       out: Collector[Drawdown]
     ): Unit = {
-    val currentMax = Option(maxValueState.value()).getOrElse(startingValue)
+    val (lastTs, lastMaxValue) = Option(maxValueState.value()).getOrElse(startingState)
 
-    val updatedMax = Math.max(currentMax, value.value)
-    maxValueState.update(updatedMax)
+    if (elem.timestamp <= lastTs) {
+      logger.info(s"Skipping out-of-order event: $elem (latest timestamp: $lastTs)")
+      return
+    }
 
-    val drawdown = (value.value / updatedMax) * 100
+    val updatedMaxValue = Math.max(lastMaxValue, elem.value)
 
-    out.collect(Drawdown(value.timestamp, value.field, value.value, drawdown))
+    val updatedTs = {
+      if (Instant.ofEpochMilli(elem.timestamp).isBefore(Instant.now().truncatedTo(ChronoUnit.DAYS)))
+        elem.timestamp
+      else
+        lastTs
+    }
+
+    maxValueState.update(updatedTs -> updatedMaxValue)
+
+    out.collect(
+      Drawdown(
+        timestamp = elem.timestamp,
+        field     = elem.field,
+        value     = elem.value,
+        drawdown  = (elem.value / updatedMaxValue) * 100
+      ))
   }
 }
