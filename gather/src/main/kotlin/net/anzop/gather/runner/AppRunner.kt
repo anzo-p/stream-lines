a0b2bf7053ff74a72d5af6c0ec56e01e95d0e64c
anzo-p <anssi.pitkan@gmail.com>
2025-03-03 21:03:11 +0200

    gather - fix app runner coroutine exception

--- gather/src/main/kotlin/net/anzop/gather/runner/AppRunner.kt
+++ gather/src/main/kotlin/net/anzop/gather/runner/AppRunner.kt
@@ -4,10 +4,12 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.withTimeoutOrNull
 import net.anzop.gather.service.BarDataFetcher
 import net.anzop.gather.service.IndexProcessor
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
+import kotlin.time.Duration.Companion.minutes
 
 @Component
 class AppRunner(
@@ -19,38 +21,39 @@ class AppRunner(
     private val mutex = Mutex()
     private val coroutineScope = CoroutineScope(Dispatchers.IO)
     @Volatile private var isRunning = false
-
-    fun fetchAndProcess(redoIndex: Boolean = false): RunnerCallResult =
+    
+    fun fetchAndProcess(redoIndex: Boolean = false): RunnerCallResult {
         if (!mutex.tryLock()) {
             logger.info("${RunnerCallResult.LOCK_UNAVAILABLE.message} - Exiting...")
-            RunnerCallResult.LOCK_UNAVAILABLE
-        } else {
-            try {
-                if (isRunning) {
-                    logger.info("${RunnerCallResult.ALREADY_RUNNING.message} - Exiting...")
-                    RunnerCallResult.ALREADY_RUNNING
-                }
+            return RunnerCallResult.LOCK_UNAVAILABLE
+        }
 
-                isRunning = true
-                logger.info("Launching tasks...")
-                coroutineScope.launch {
-                    try {
-                        if (!redoIndex) {
-                            barDataFetcher.run()
-                        }
-                        indexProcessor.run()
-                        logger.info("AppRunner.fetchAndProcess tasks completed.")
-                    } finally {
-                        isRunning = false
-                        mutex.unlock()
+        if (isRunning) {
+            logger.info("${RunnerCallResult.ALREADY_RUNNING.message} - Exiting...")
+            mutex.unlock()
+            return RunnerCallResult.ALREADY_RUNNING
+        }
+
+        isRunning = true
+        logger.info("Launching tasks...")
+
+        coroutineScope.launch {
+            withTimeoutOrNull(15.minutes) {
+                try {
+                    if (!redoIndex) {
+                        barDataFetcher.run()
                     }
+                    indexProcessor.run()
+                    logger.info("AppRunner.fetchAndProcess coroutine tasks completed.")
+                } catch (e: Exception) {
+                    logger.error("AppRunner.fetchAndProcess coroutine failed.", e)
+                } finally {
+                    isRunning = false
+                    mutex.unlock()
                 }
-
-                RunnerCallResult.SUCCESS
-            } catch (e: Exception) {
-                isRunning = false
-                mutex.unlock()
-                throw e
             }
         }
+
+        return RunnerCallResult.SUCCESS
+    }
 }
