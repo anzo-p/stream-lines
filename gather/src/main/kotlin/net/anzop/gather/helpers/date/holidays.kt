860e04d8af56c9634506fc7851ad268a31166c51
anzo-p <anssi.pitkan@gmail.com>
2025-05-24 21:57:45 +0300

    gather - irregular holidays

--- gather/src/main/kotlin/net/anzop/gather/helpers/date/holidays.kt
+++ gather/src/main/kotlin/net/anzop/gather/helpers/date/holidays.kt
@@ -3,9 +3,24 @@ package net.anzop.gather.helpers.date
 import java.time.DayOfWeek
 import java.time.LocalDate
 
-private data class VariableHoliday(val month: Int, val dayOfWeek: DayOfWeek, val n: Int)
+private data class IrregularHoliday(val date: LocalDate)
 
-private val fixedHolidays: Set<Pair<Int, Int>> = listOf(
+private data class VariableRegularHoliday(val month: Int, val dayOfWeek: DayOfWeek, val n: Int)
+
+private val irregularHolidays: Set<IrregularHoliday> = setOf(
+    IrregularHoliday(LocalDate.of(2016, 3, 25)), // Good Friday
+    IrregularHoliday(LocalDate.of(2017, 4, 14)), // Good Friday
+    IrregularHoliday(LocalDate.of(2018, 3, 30)), // Good Friday
+    IrregularHoliday(LocalDate.of(2019, 4, 19)), // Good Friday
+    IrregularHoliday(LocalDate.of(2020, 4, 10)), // Good Friday
+    IrregularHoliday(LocalDate.of(2021, 4, 2)),  // Good Friday
+    IrregularHoliday(LocalDate.of(2022, 4, 15)), // Good Friday
+    IrregularHoliday(LocalDate.of(2023, 4, 7)),  // Good Friday
+    IrregularHoliday(LocalDate.of(2024, 3, 29)), // Good Friday
+    IrregularHoliday(LocalDate.of(2025, 4, 18)), // Good Friday
+)
+
+private val fixedRegularHolidays: Set<Pair<Int, Int>> = setOf(
     "1.1", //"New Year's Day
     "19.6", // Juneteenth National Independence Day
     "4.7", // Independence Day
@@ -19,17 +34,17 @@ private val fixedHolidays: Set<Pair<Int, Int>> = listOf(
     }
     .toSet()
 
-private val variableHolidays: Set<VariableHoliday> = setOf(
-    VariableHoliday(1, DayOfWeek.MONDAY, 3), // Martin Luther King Jr. Day
-    VariableHoliday(2, DayOfWeek.MONDAY, 3), // Washington's Birthday
-    VariableHoliday(5, DayOfWeek.MONDAY, -1), // Memorial Day
-    VariableHoliday(9, DayOfWeek.MONDAY, 1), // Labor Day
-    VariableHoliday(10, DayOfWeek.MONDAY, 2), // Columbus Day
-    VariableHoliday(11, DayOfWeek.THURSDAY, 4), // Thanksgiving Day
+private val variableHolidays: Set<VariableRegularHoliday> = setOf(
+    VariableRegularHoliday(1, DayOfWeek.MONDAY, 3),    // Martin Luther King Jr. Day
+    VariableRegularHoliday(2, DayOfWeek.MONDAY, 3),    // Washington's Birthday
+    VariableRegularHoliday(5, DayOfWeek.MONDAY, -1),   // Memorial Day
+    VariableRegularHoliday(9, DayOfWeek.MONDAY, 1),    // Labor Day
+    VariableRegularHoliday(10, DayOfWeek.MONDAY, 2),   // Columbus Day
+    VariableRegularHoliday(11, DayOfWeek.THURSDAY, 4), // Thanksgiving Day
 )
 
 fun LocalDate.isHoliday(): Boolean {
-    val fixed = fixedHolidays.contains(this.dayOfMonth to this.monthValue)
+    val fixed = fixedRegularHolidays.contains(this.dayOfMonth to this.monthValue)
 
     val variable = variableHolidays.any { (month, dayOfWeek, n) ->
         this.monthValue == month && this.dayOfWeek == dayOfWeek && run {
@@ -41,5 +56,7 @@ fun LocalDate.isHoliday(): Boolean {
         }
     }
 
-    return fixed || variable
+    val irregular = irregularHolidays.any { it.date == this }
+
+    return fixed || variable || irregular
 }
