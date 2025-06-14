ae9287fc5385027e2d7f699a2c5c602eae29b380
anzo-p <anssi.pitkan@gmail.com>
2025-06-14 13:18:35 +0300

    gather - improve validation

--- gather/src/main/kotlin/net/anzop/gather/helpers/jakarta/validator.kt
+++ gather/src/main/kotlin/net/anzop/gather/helpers/jakarta/validator.kt
@@ -1,13 +1,18 @@
 package net.anzop.gather.helpers.jakarta
 
-import jakarta.validation.ConstraintViolation
 import jakarta.validation.Validation
-import jakarta.validation.Validator
-import jakarta.validation.ValidatorFactory
 
-private val validatorFactory: ValidatorFactory = Validation.buildDefaultValidatorFactory()
-private val validator: Validator = validatorFactory.validator
-
-fun <T> validate(obj: T): Set<ConstraintViolation<T>> {
-    return validator.validate(obj)
+inline fun <reified T> validate(obj: T) {
+    Validation
+        .buildDefaultValidatorFactory()
+        .validator
+        .validate(obj)
+        .takeIf { it.isNotEmpty() }
+        ?.let { violations ->
+            val errorMessages = violations.joinToString("; ") { violation ->
+                val propertyName = violation.propertyPath.iterator().asSequence().last().name
+                "'$propertyName' ${violation.message}"
+            }
+            throw IllegalArgumentException("Validation failed for ${T::class.simpleName}: $errorMessages")
+        }
 }
