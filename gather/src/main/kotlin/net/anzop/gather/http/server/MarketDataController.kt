d9315ae943445d623808b1f6e31215c93a569e8c
anzo-p <anssi.pitkan@gmail.com>
2025-11-10 09:11:41 +0200

    gather - new endpoint get top 30 tickers

---
+++ gather/src/main/kotlin/net/anzop/gather/http/server/MarketDataController.kt
@@ -0,0 +1,55 @@
+package net.anzop.gather.http.server
+
+import java.math.BigDecimal
+import java.math.RoundingMode
+import java.time.YearMonth
+import net.anzop.gather.config.SourceDataConfig
+import net.anzop.gather.helpers.date.asAmericaNyToInstant
+import net.anzop.gather.model.marketData.BarData
+import net.anzop.gather.model.marketData.Measurement
+import net.anzop.gather.repository.dynamodb.TradingVolumesRepository
+import net.anzop.gather.repository.influxdb.MarketDataFacade
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/market-data")
+class MarketDataController(
+    private val marketDataFacade: MarketDataFacade,
+    private val sourceDataConfig: SourceDataConfig,
+    private val tradingVolumesRepository: TradingVolumesRepository,
+) {
+    @GetMapping("/rank-by-trading")
+    fun fetchFinancials(): ResponseEntity<Map<String, Map<String, BigDecimal>>> {
+        val ym = YearMonth.now().minusMonths(1)
+        val key = "%04d-%02d".format(ym.year, ym.monthValue)
+        val tradingVolumes = tradingVolumesRepository.getTradingVolumes(key)
+            .takeIf { it.isNotEmpty() }
+            ?: service(ym).also { fresh ->
+                tradingVolumesRepository.storeTradingVolumes(mapOf(key to fresh))
+            }
+
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(mapOf(key to tradingVolumes))
+    }
+
+    private fun service(ym: YearMonth): Map<String, BigDecimal> = sourceDataConfig
+        .params
+        .map { it.marketData.ticker }
+        .associateWith { ticker ->
+            marketDataFacade.getMeasurements(
+                measurement = Measurement.SECURITY_RAW_SEMI_HOURLY,
+                from = ym.atDay(1).asAmericaNyToInstant(),
+                til  = ym.atEndOfMonth().asAmericaNyToInstant(),
+                ticker = ticker,
+                clazz = BarData::class.java
+            )
+                .sumOf { it.totalTradingValue }
+                .let { BigDecimal.valueOf(it) }
+                .setScale(2, RoundingMode.HALF_UP)
+        }
+}
---
+++ gather/src/main/kotlin/net/anzop/gather/repository/dynamodb/TradingVolumesRepository.kt
@@ -0,0 +1,43 @@
+package net.anzop.gather.repository.dynamodb
+
+import java.math.BigDecimal
+import net.anzop.gather.config.AwsConfig
+import org.springframework.stereotype.Repository
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue
+
+@Repository
+class TradingVolumesRepository(
+    awsConfig: AwsConfig,
+    dynamoDbClient: DynamoDbClient,
+) : DynamoDbCore(awsConfig, dynamoDbClient) {
+
+    fun storeTradingVolumes(tradingVolumes: Map<String, Map<String, BigDecimal>>) =
+        tradingVolumes.forEach { (key, volumes) ->
+            saveItem(
+                mapOf(
+                    "PK" to "monthlyTradingVolumes".toAttrib(),
+                    "SK" to "year-month#${key}".toAttrib(),
+                    "tradingVolumes" to volumes.toAttrib()
+                )
+            )
+        }
+
+    fun getTradingVolumes(key: String): Map<String, BigDecimal> {
+        val key = mapOf(
+            "PK" to "monthlyTradingVolumes".toAttrib(),
+            "SK" to "year-month#${key}".toAttrib(),
+        )
+        return getItem(key)
+            ?.let { it["tradingVolumes"]?.toMapBigDecimalValues() }
+            ?: mutableMapOf()
+    }
+
+    private fun Map<String, BigDecimal>.toAttrib(): AttributeValue =
+        AttributeValue.builder().m(
+            this.mapValues { AttributeValue.builder().n(it.value.toString()).build() }
+        ).build()
+
+    private fun AttributeValue?.toMapBigDecimalValues(): Map<String, BigDecimal> =
+        this?.m()?.mapValues { (_, v) -> BigDecimal(v.n()) } ?: emptyMap()
+}
